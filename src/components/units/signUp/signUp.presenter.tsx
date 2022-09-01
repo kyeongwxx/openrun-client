81ea83aa6f914f01b2448a6ef41ae81eee234b2b
kyeongwxx <kyeongwxx@gmail.com>
2022-09-02 00:42:30 +0900

    [FEAT] 회원가입페이지-퍼블리싱 데스크탑

--- src/components/units/signUp/signUp.presenter.tsx
+++ src/components/units/signUp/signUp.presenter.tsx
@@ -1,3 +1,40 @@
-export default function SignUpUI(){
-    return <></>
+import * as s from "./signUp.styles";
+
+export default function SignUpUI() {
+  return (
+    <s.Wrapper>
+      <s.Title>회원가입</s.Title>
+      <s.Wrapper01>
+        <s.Input01 placeholder="아이디" type="text"></s.Input01>
+        <s.Btn01>중복확인</s.Btn01>
+      </s.Wrapper01>
+      <s.Wrapper01>
+        <s.Input01 placeholder="휴대전화" type="text"></s.Input01>
+        <s.Btn01>인증하기</s.Btn01>
+      </s.Wrapper01>
+      <s.Input02 placeholder="인증번호" type="text"></s.Input02>
+      <s.Input02 placeholder="비밀번호" type="password"></s.Input02>
+      <s.Input02 placeholder="비밀번호 확인" type="password"></s.Input02>
+      <s.AgreeWrapper>
+        <s.AgreeBtn></s.AgreeBtn>
+        <s.AgreeText>전체동의</s.AgreeText>
+      </s.AgreeWrapper>
+      <s.DivideLine></s.DivideLine>
+      <s.EssentialWrapper>
+        <s.BtnWrapper>
+          <s.EssentialBtn></s.EssentialBtn>
+          <s.EssentialText>(필수) 개인정보 수집 및 이용 동의</s.EssentialText>
+        </s.BtnWrapper>
+        <s.DetailText>자세히 보기</s.DetailText>
+      </s.EssentialWrapper>
+      <s.SelectWrapper>
+        <s.BtnWrapper>
+          <s.SelectBtn></s.SelectBtn>
+          <s.SelectText>(선택) 이벤트 소식 등 알림 정보 받기</s.SelectText>
+        </s.BtnWrapper>
+        <s.DetailText>자세히 보기</s.DetailText>
+      </s.SelectWrapper>
+      <s.SignUpBtn>회원가입하기</s.SignUpBtn>
+    </s.Wrapper>
+  );
 }
--- src/components/units/signUp/signUp.styles.ts
+++ src/components/units/signUp/signUp.styles.ts
@@ -1,1 +1,125 @@
 import styled from "@emotion/styled";
+
+export const Wrapper = styled.main`
+  width: 95%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const Title = styled.div`
+  font-size: 1.875rem;
+  color: #000000;
+  padding-top: 4.375rem;
+  padding-bottom: 3.125rem;
+`;
+export const Wrapper01 = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 1rem;
+`;
+export const Input01 = styled.input`
+  width: 16.8%;
+  height: 77px;
+  background: #f7f7f7;
+  border-radius: 24px;
+  font-size: 0.938rem;
+  color: #000000;
+  border: none;
+  padding-left: 1.625rem;
+  margin-right: 0.813rem;
+`;
+export const Btn01 = styled.button`
+  width: 6.8%;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 24px;
+  border: none;
+`;
+export const Input02 = styled.input`
+  width: 24.4%;
+  height: 77px;
+  background: #f7f7f7;
+  border-radius: 24px;
+  border: none;
+  padding-left: 1.625rem;
+  margin-bottom: 1rem;
+  color: #000000;
+`;
+export const AgreeWrapper = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  padding-top: 1.438rem;
+  padding-bottom: 0.938rem;
+  color: #393939;
+  font-weight: 400;
+  font-size: 0.813rem;
+`;
+export const AgreeBtn = styled.div`
+  width: 18px;
+  height: 18px;
+  border-radius: 9px;
+  border: 1px solid #393939;
+  background: white;
+  margin-right: 0.5rem;
+`;
+export const AgreeText = styled.div``;
+export const DivideLine = styled.div`
+  width: 25%;
+  border-bottom: 1px solid rgba(101, 101, 101, 0.5);
+`;
+export const EssentialWrapper = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 1.313rem;
+  color: #393939;
+  font-weight: 400;
+  font-size: 0.813rem;
+`;
+export const BtnWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const EssentialBtn = styled.div`
+  width: 18px;
+  height: 18px;
+  border-radius: 9px;
+  border: 1px solid #393939;
+  background: white;
+  margin-right: 0.5rem;
+`;
+export const EssentialText = styled.div``;
+export const DetailText = styled.div``;
+export const SelectWrapper = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 1.313rem;
+  color: #393939;
+  font-weight: 400;
+  font-size: 0.813rem;
+  padding-bottom: 2.75rem;
+`;
+export const SelectBtn = styled.div`
+  width: 18px;
+  height: 18px;
+  border-radius: 9px;
+  border: 1px solid #393939;
+  background: white;
+  margin-right: 0.5rem;
+`;
+export const SelectText = styled.div``;
+export const SignUpBtn = styled.button`
+  width: 24.4%;
+  height: 77px;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 24px;
+  border: none;
+  margin-bottom: 4.125rem;
+`;
