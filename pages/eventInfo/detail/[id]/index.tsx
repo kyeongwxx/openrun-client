c3e9951dce3d0bc9a01e2a3330311c8f5dbecee2
kyeongwxx <kyeongwxx@gmail.com>
2022-09-01 15:43:31 +0900

    [FEAT] 테스트 배포

--- pages/eventInfo/detail/[id]/index.tsx
+++
@@ -1,5 +0,0 @@
-import EventInfoDetail from "../../../../src/components/units/eventInfo/detail/eventInfoDetail.container";
-
-export default function EventInfoDetailPage(){
-    return <EventInfoDetail />
-}
