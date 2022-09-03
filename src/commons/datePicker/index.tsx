fa4c5db1ce4a4a0097945ffb4c24be5fb865e905
HYUN D <hyunzilee@HYUNui-MacBookAir.local>
2022-09-03 11:52:36 +0900

    [ADD] 메인페이지 반응형

--- src/commons/datePicker/index.tsx
+++ src/commons/datePicker/index.tsx
@@ -4,6 +4,7 @@ import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { useState } from "react";
 import { currentDate } from "../function/currentDate";
 import { v4 as uuidv4 } from "uuid";
+import { useMediaQuery } from "react-responsive";
 
 export default function DatePicker() {
   const oneWeek = currentDate();
@@ -11,21 +12,39 @@ export default function DatePicker() {
   const date = [...oneWeek[0]];
   const day = [...oneWeek[1]];
 
+  const isPc = useMediaQuery({
+    query: "(min-width:1200px)",
+  });
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1199px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
+
   return (
     <s.Wrapper>
-      <s.Carousel>
-        <s.ButtonWrapper>
-          <ArrowLeftIcon style={{ fontSize: "40px", cursor: "pointer" }} />
-          {date.map((el, index) => (
-            <s.Button key={uuidv4()}>
-              <s.TextDay>{day[index]}</s.TextDay>
-              <s.TextDate>{el}</s.TextDate>
-            </s.Button>
-          ))}
+      {isPc && (
+        <s.Carousel>
+          <s.ButtonWrapper>
+            <ArrowLeftIcon style={{ fontSize: "40px", cursor: "pointer" }} />
+            {date.map((el, index) => (
+              <s.Button key={uuidv4()}>
+                <s.TextDay>{day[index]}</s.TextDay>
+                <s.TextDate>{el}</s.TextDate>
+              </s.Button>
+            ))}
 
-          <ArrowRightIcon style={{ fontSize: "40px", cursor: "pointer" }} />
-        </s.ButtonWrapper>
-      </s.Carousel>
+            <ArrowRightIcon style={{ fontSize: "40px", cursor: "pointer" }} />
+          </s.ButtonWrapper>
+        </s.Carousel>
+      )}
+      {(isTablet || isMobile) && (
+        <s.ButtonMobile>
+          {/* <s.TextDay>sdf</s.TextDay> */}
+          <s.TextDate>Today</s.TextDate>
+        </s.ButtonMobile>
+      )}
     </s.Wrapper>
   );
 }
