c743c29babbb8765b4b299448e3cc3d8c8be3e85
SonJjiFD <songjifd@gmail.com>
2022-09-03 20:54:44 +0900

    랜딩페이지 애니메이션효과완료

--- src/components/units/rendering/rendering.presenter.tsx
+++ src/components/units/rendering/rendering.presenter.tsx
@@ -1,3 +1,44 @@
+import * as s from "./rendering.styles";
+
 export default function RenderingUI() {
-  return <></>;
+  return (
+    <s.Wrapper>
+      <s.EventWrapper>
+        <s.Box1>
+          <s.ItemImg src="/img/shoes.png" />
+
+          <s.TextBox>
+            <s.TextLine1>
+              <s.Text1>사고싶은</s.Text1>
+              <s.Text2>한정판</s.Text2>
+              <s.Text1>운동화</s.Text1>
+            </s.TextLine1>
+            <s.TextLine2>연차내고 줄서기는 부담스러우신가요?</s.TextLine2>
+          </s.TextBox>
+        </s.Box1>
+        <s.Box2>
+          <s.TextBox>
+            <s.TextLine1>
+              <s.Text1>사고싶은</s.Text1>
+              <s.Text2>한정판</s.Text2>
+              <s.Text1>운동화</s.Text1>
+            </s.TextLine1>
+            <s.TextLine2>연차내고 줄서기는 부담스러우신가요?</s.TextLine2>
+          </s.TextBox>
+          <s.ItemImg2 src="/img/shoes2.png" />
+        </s.Box2>
+        <s.Box3>
+          <s.ItemImg3 src="/img/shoes2.png" />
+          <s.TextBox>
+            <s.TextLine1>
+              <s.Text1>사고싶은</s.Text1>
+              <s.Text2>한정판</s.Text2>
+              <s.Text1>운동화</s.Text1>
+            </s.TextLine1>
+            <s.TextLine2>연차내고 줄서기는 부담스러우신가요?</s.TextLine2>
+          </s.TextBox>
+        </s.Box3>
+      </s.EventWrapper>
+    </s.Wrapper>
+  );
 }
--- src/components/units/rendering/rendering.styles.ts
+++ src/components/units/rendering/rendering.styles.ts
@@ -1,1 +1,107 @@
 import styled from "@emotion/styled";
+import "animate.css";
+
+export const Wrapper = styled.main`
+  width: 95%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const EventWrapper = styled.div`
+  width: 100%;
+  //height: 100rem;
+  display: flex;
+  flex-direction: column;
+  //border: 1px solid red;
+`;
+export const Box1 = styled.div`
+  width: 100%;
+  height: 40%;
+
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: row;
+  //border: 1px solid blue;
+`;
+export const Box2 = styled.div`
+  width: 100%;
+  height: 40%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  //border: 1px solid green;
+`;
+export const Box3 = styled.div`
+  width: 100%;
+  height: 40%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 12rem;
+  // border: 1px solid green;
+`;
+export const ItemImg = styled.img`
+  width: 50rem;
+  animation: fadeInLeft;
+  animation-duration: 2s;
+`;
+export const ItemImg2 = styled.img`
+  width: 50rem;
+  animation: fadeInRight;
+  animation-duration: 5s;
+
+  // border: 1px solid green;
+`;
+export const ItemImg3 = styled.img`
+  width: 50rem;
+  animation: fadeInLeft;
+  animation-duration: 8s;
+`;
+
+export const TextBox = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  // border: 1px solid red;
+`;
+export const TextLine1 = styled.div`
+  margin-top: 8rem;
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  font-weight: 800;
+`;
+
+export const Text1 = styled.div`
+  padding-right: 1rem;
+  padding-left: 1rem;
+`;
+export const Text2 = styled.div`
+  color: #3f00fd;
+  font-size: 3rem;
+`;
+export const TextLine2 = styled.div`
+  font-size: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const EventWrapper2 = styled.div`
+  width: 60%;
+  height: 30%;
+  display: flex;
+  flex-direction: row;
+`;
