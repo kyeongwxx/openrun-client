258f608f8d0b180f487466dfc7d23d301bc0a705
SonJjiFD <songjifd@gmail.com>
2022-09-04 20:44:34 +0900

    행사정보 상세페이지 퍼블리싱

--- src/components/units/eventInfo/detail/eventInfoDetail.styles.ts
+++ src/components/units/eventInfo/detail/eventInfoDetail.styles.ts
@@ -1,1 +1,178 @@
 import styled from "@emotion/styled";
+
+export const Wrapper = styled.main`
+  width: 95%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ItemWrapper = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: row;
+  padding-top: 3rem;
+  padding-bottom: 2rem;
+`;
+export const ItemImg = styled.img`
+  width: 40%;
+  height: 100%;
+`;
+export const ItemInfoBox = styled.div`
+  margin-left: 10%;
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+  width: 100%;
+`;
+export const PlanTitle = styled.div`
+  font-size: 0.85rem;
+  font-weight: 800;
+  padding-bottom: 1rem;
+  color: #9b9b9b;
+`;
+export const Brand = styled.div`
+  font-size: 0.9rem;
+  font-weight: 500;
+  padding-bottom: 0.7rem;
+`;
+export const ItemEventImg = styled.div``;
+export const Event = styled.div`
+  font-size: 1.1rem;
+  font-weight: 650;
+  padding-bottom: 1rem;
+`;
+export const Time = styled.div`
+  font-size: 0.95rem;
+  font-weight: 350;
+  padding-bottom: 1.9rem;
+  color: #555;
+`;
+export const PlaceTitle = styled.div`
+  font-size: 0.85rem;
+  font-weight: 800;
+  padding-bottom: 1rem;
+  color: #9b9b9b;
+`;
+
+export const Store = styled.div`
+  font-size: 1.05rem;
+  font-weight: 600;
+  padding-bottom: 0.5rem;
+  text-decoration: underline;
+`;
+export const StoreDetail = styled.div`
+  font-size: 0.9rem;
+  font-weight: 400;
+  padding-bottom: 1rem;
+  color: #808080;
+`;
+export const PriceBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: flex-end;
+`;
+export const Price = styled.div`
+  font-size: 1.3rem;
+  font-weight: 400;
+`;
+
+export const DivideLine = styled.div`
+  width: 75%;
+  border-bottom: 2px solid rgba(101, 101, 101, 0.5); ;
+`;
+
+export const InfoWrapper = styled.div`
+  margin-top: 5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const InfoTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: 700;
+
+  margin-bottom: 3rem;
+`;
+export const Information = styled.div`
+  margin-bottom: 6rem;
+  font-size: 0.8rem;
+  text-align: center;
+`;
+export const InfoImg1 = styled.img`
+  width: 70%;
+  margin-bottom: 2rem;
+`;
+export const InfoImg2 = styled.img`
+  width: 65%;
+`;
+export const ImgBox = styled.div`
+  width: 55%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`;
+export const InfoImg3 = styled.img`
+  width: 45%;
+`;
+export const InfoImg4 = styled.img`
+  width: 45%;
+`;
+export const InfoImg5 = styled.img`
+  width: 45%;
+`;
+export const MoveToSiteBtn = styled.button`
+  width: 35%;
+  height: 55px;
+  font-size: 0.95rem;
+  font-weight: 500;
+  border-radius: 15px;
+  background: #2e2e2e;
+  border: none;
+
+  margin-top: 5rem;
+  margin-bottom: 6rem;
+  color: white;
+  cursor: pointer;
+`;
+export const WithItemWrapper = styled.div`
+  text-align: center;
+`;
+export const WithItemTitle = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+
+  margin-bottom: 1rem;
+`;
+export const WithItemSubTitle = styled.div`
+  font-size: 1.5rem;
+  color: #878787;
+  font-weight: 350;
+
+  margin-bottom: 3rem;
+`;
+export const WithItemBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 6rem;
+`;
+export const WithItemImg = styled.img`
+  width: 20%;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+
+  cursor: pointer;
+`;
+// export const NewImg = styled.img`
+//   width: 3%;
+// `;
