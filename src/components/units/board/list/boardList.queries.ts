5bb2923b76956a80e6ca12982c0c8cedc9acf6d5
kyeongwxx <kyeongwxx@gmail.com>
2022-09-08 08:21:37 +0900

    [ADD] boardList 페이지 - fetchBoards 데이터 조회

--- src/components/units/board/list/boardList.queries.ts
+++ src/components/units/board/list/boardList.queries.ts
@@ -1,1 +1,27 @@
-import styled from "@emotion/styled";
+import { gql } from "@apollo/client";
+
+export const FETCH_BOARDS = gql`
+  query fetchBoards($dateType: String!, $page: Int) {
+    fetchBoards(dateType: $dateType, page: $page) {
+      id
+      title
+      contents
+      # status
+      price
+      eventDay
+      eventTime
+      user {
+        nickName
+      }
+      category {
+        name
+      }
+      location {
+        address
+      }
+      image {
+        url
+      }
+    }
+  }
+`;
--- src/components/units/comment/list/commentList.presenterItem.tsx
+++
@@ -1,3 +0,0 @@
-export default function CommentListUIItem() {
-  return <></>;
-}
