fad87e08a343bd1c68af7560b8855232c086b8d2
kyeongwxx <kyeongwxx@gmail.com>
2022-09-19 15:56:10 +0900

    [MODIFY] 반응형 수정

--- src/commons/yup/boardWrite/index.ts
+++ src/commons/yup/boardWrite/index.ts
@@ -7,8 +7,8 @@ export const schema = yup.object({
     .required("대행 가격을 입력해주세요.")
     .typeError("대행 가격(숫자)을 입력해주세요.")
     .positive("0이상의 숫자를 입력해주세요."),
-  address: yup.string().required("주소를 입력(수정)해주세요"),
-  addressDetail: yup.string().required("상세 주소를 입력(수정)해주세요."),
-  contents: yup.string().required("요청사항을 입력(수정)해주세요."),
-  image: yup.array().required("이미지를 업로드(수정)해주세요."),
+  address: yup.string().required("주소를 입력해주세요"),
+  addressDetail: yup.string().required("상세 주소를 입력해주세요."),
+  contents: yup.string().required("요청사항을 입력해주세요."),
+  image: yup.array().required("이미지를 업로드해주세요."),
 });
