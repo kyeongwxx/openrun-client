b7a100ec6aac4205ea41d193cd7072ff9681947e
kyeongwxx <kyeongwxx@gmail.com>
2022-10-07 18:55:34 +0900

    [MODIFY] boardList 페이지 typescript

--- src/components/units/board/list/boardList.types.ts
+++ src/components/units/board/list/boardList.types.ts
@@ -1,3 +1,6 @@
+import { ApolloQueryResult } from "@apollo/client";
+import { NextRouter } from "next/router";
+import { ChangeEvent } from "react";
 import { IQuery } from "../../../../commons/types/generated/types";
 
 export interface BoardListUIProps {
@@ -7,13 +10,24 @@ export interface BoardListUIProps {
   isMobile: boolean;
   isUltra: boolean;
   keyword: any;
+  router: NextRouter;
   onClickMoveToAll: () => void;
   onClickMoveToFashion: () => void;
   onClickMoveToAcc: () => void;
   onClickMoveToDigital: () => void;
   onClickMoveToFood: () => void;
   onClickMoveToToy: () => void;
-  onChangeSearch: () => void;
+  onChangeSearch: (event: ChangeEvent<HTMLInputElement>) => void;
   onClickMoveToProductDetail: (e: any) => void;
   onFetchMore: () => void;
+  refetch: (
+    variables?:
+      | Partial<{
+          dateType: string;
+          direcion: string;
+          search: string;
+          page: number;
+        }>
+      | undefined
+  ) => Promise<ApolloQueryResult<any>>;
 }
